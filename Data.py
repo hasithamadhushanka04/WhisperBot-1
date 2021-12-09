f5ef5eca227c911e134d049e2e6024389ae9f1c2
Hasitha Madhushanka <90751622+hasithamadhushanka04@users.noreply.github.com>
2021-12-09 13:31:42 +0530

    Update Data.py

--- Data.py
+++ Data.py
@@ -7,14 +7,14 @@ class Data:
 Hey {}.
 Welcome to {}
 
-I am the Master of Whisperers (like Varys in Game of Thrones).
+A TeleGram Bot to send messages to a person secretly!
 
 You can use me to send whispers to your friend in groups and channels (even if I'm not there).
 Only that friend and you will be able to read the message even though others are in same group. 
 
 To see how to use me press 'How to Use' below.
 
-By @StarkBots
+By @CloudUpdateslk
     """
 
     # Home Button
@@ -31,8 +31,7 @@ By @StarkBots
             InlineKeyboardButton("How to Use ❔", callback_data="help"),
             InlineKeyboardButton("🎪 About 🎪", callback_data="about")
         ],
-        [InlineKeyboardButton("♥ More Amazing bots ♥", url="https://t.me/StarkBots")],
-        [InlineKeyboardButton("🎨 Support Group 🎨", url="https://t.me/StarkBotsChat")],
+        [InlineKeyboardButton("updates channel", url="https://t.me/CloudUpdateslk")],
     ]
 
     # Help Message
@@ -44,17 +43,5 @@ Just type the message in below format in any chat.
 
     # About Message
     ABOUT = """
-**About This Bot** 
-
-Bot created by @StarkBots
-
-Source Code : [Click Here](https://github.com/StarkBotsIndustries/WhisperBot)
-
-Inspired By : nnbbot
-
-Framework : [Pyrogram](docs.pyrogram.org)
-
-Language : [Python](www.python.org)
-
-Developer : @StarkProgrammer
+hi
     """
